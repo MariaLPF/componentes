217737e05eb3cda968369e938d9c701f1110a1a3
María Pardo <liriopf@hotmail.com>
2019-12-27 09:49:11 +0100

    creamos un card con una imagen

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CardPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          title: Text('Cards'),
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(10.0),
-          children: <Widget>[
-            _cardType1()
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cardType1(), SizedBox(height: 30), _cardType2()],
+      ),
     );
   }
 
@@ -22,26 +19,43 @@ class CardPage extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            leading: Icon(Icons.photo_album, color: Colors.blueGrey,),
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blueGrey,
+            ),
             title: Text('Soy el titulo de esta targeta'),
-            subtitle: Text('Esto es una descripción de una targeta para ver una descripcion de lo que queremos ver'),
+            subtitle: Text(
+                'Esto es una descripción de una targeta para ver una descripcion de lo que queremos ver'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               FlatButton(
                 child: Text('Cancelar'),
-                onPressed: (){
-
-                },
+                onPressed: () {},
               ),
               FlatButton(
                 child: Text('Ok'),
-                onPressed: (){
-                  
-                },
+                onPressed: () {},
               )
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/7/78/Frosty_Raftsundet_landscape_with_Trolltindan_in_morning%2C_2012_October.JPG'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Paisaje')
           )
         ],
       ),
