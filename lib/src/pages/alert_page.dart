a7fc411c9f4d591382bd3e8c7cef895146e21a84
María Pardo <liriopf@hotmail.com>
2019-12-27 11:58:24 +0100

    AlertDialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          textColor: Colors.white,
+          color: Colors.blue,
+          shape: StadiumBorder(),
+          onPressed: ()=>_showAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.redo),
-        onPressed: (){
+        onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+            title: Text('Aviso'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Esto es un aviso de algo importante, hagale caso, no lo ignore'),
+                SizedBox(height: 15.0),
+                FlutterLogo(size:50.0),
+              ],
+            ),
+            elevation: 30.0,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+            
+        );
+      }
     );
   }
 }
